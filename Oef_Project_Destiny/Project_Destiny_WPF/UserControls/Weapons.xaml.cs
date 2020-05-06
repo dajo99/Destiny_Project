77d6b6bce3ac86bdf70f47ad40dc0afbc97520e9
KevinBelien <K3vin.belien@gmail.com>
2020-05-06 23:47:05 +0200

    branch feature-Weapons aangemaakt + first release lay-out

---
+++ Oef_Project_Destiny/Project_Destiny_WPF/UserControls/Weapons.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_Destiny_WPF.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Weapons.xaml
+    /// </summary>
+    public partial class Weapons : UserControl
+    {
+        public Weapons()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmbCategorie_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbZeldzaamheid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void dbWapens_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
