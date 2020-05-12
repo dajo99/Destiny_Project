979d1b98c7b92cf90ac2b549a9f539a581fdcb90
KevinBelien <K3vin.belien@gmail.com>
2020-05-12 09:02:32 +0200

    partial class account added to DAL

---
+++ Oef_Project_Destiny/Destiny_DAL/Partials/Account.cs
@@ -0,0 +1,23 @@
+﻿using Destiny_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destiny_DAL.Partials
+{
+    public partial class Account : Basisklasse
+    {
+        public override string this[string columnName]
+        {
+            get
+            {
+                if (columnName == "Accountnaam" && string.IsNullOrWhiteSpace(Accountnaam))
+                {
+
+                }
+            }
+        }
+    }
+}
