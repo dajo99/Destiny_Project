8260647b3fbf541bfc96acc379613d1e471d84c5
KevinBelien <K3vin.belien@gmail.com>
2020-05-13 06:18:58 +0200

    Encryption update, newer version

--- Oef_Project_Destiny/Destiny_Models/SecurePassword.cs
+++ Oef_Project_Destiny/Destiny_Models/SecurePassword.cs
@@ -36,10 +36,10 @@ namespace Destiny_Models
         public static string EncryptString(string encr)
         {
             byte[] data = UTF8Encoding.UTF8.GetBytes(encr);
-            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            using (SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider())
             {
-                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes("892C8E496E1E33355E858B327B@C238A939B7601E96159F9E9CAD05@19BA5055CD"));
-                using (TripleDESCryptoServiceProvider tripdes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                byte[] keys = sha256.ComputeHash(UTF8Encoding.UTF8.GetBytes("892C8E496E1E33355E858B327B@C238A939B7601E96159F9E9CAD05@19BA5055CD"));
+                using (AesCryptoServiceProvider tripdes = new AesCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.ISO10126 })
                 {
                     ICryptoTransform transform = tripdes.CreateEncryptor();
                     byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
@@ -53,20 +53,20 @@ namespace Destiny_Models
             try
             {
                 byte[] data = Convert.FromBase64String(decr);
-                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                using (SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider())
                 {
-                    byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes("892C8E496E1E33355E858B327B@C238A939B7601E96159F9E9CAD05@19BA5055CD"));
-                    using (TripleDESCryptoServiceProvider tripdes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                    byte[] keys = sha256.ComputeHash(UTF8Encoding.UTF8.GetBytes("892C8E496E1E33355E858B327B@C238A939B7601E96159F9E9CAD05@19BA5055CD"));
+                    using (AesCryptoServiceProvider tripdes = new AesCryptoServiceProvider { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.ISO10126 })
                     {
                         ICryptoTransform transform = tripdes.CreateDecryptor();
                         byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
-                        return UTF8Encoding.UTF8.GetString(results); ;
+                        return UTF8Encoding.UTF8.GetString(results); 
                     }
                 }
             }
-            catch (FormatException fe)
-            {
-                return null;
+            catch (FormatException)
+            {  
+                return "";
             }
             
         }
