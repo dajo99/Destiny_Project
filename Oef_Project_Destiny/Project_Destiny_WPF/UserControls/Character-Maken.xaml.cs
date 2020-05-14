9b6817c040e43b3fca323d75d436ce0f153a871a
Gilles98 <guigilles@gmail.com>
2020-05-14 02:49:41 +0200

    code behind character-aanmaken + xaml has been updated

--- Oef_Project_Destiny/Project_Destiny_WPF/UserControls/Character-Maken.xaml.cs
+++ Oef_Project_Destiny/Project_Destiny_WPF/UserControls/Character-Maken.xaml.cs
@@ -25,179 +25,119 @@ namespace Project_Destiny_WPF.UserControls
         {
             InitializeComponent();
         }
-        /*
-        ///heb dit zo moeten doen omdat wij in de database een misrekening hadden gemaakt destijds met de tabellen
-        Destiny_DAL.CharacterCustomization customization = null;
 
-        ///dit gaat ervoor zorgen dat de comboboxen de juiste waardes gaan krijgen en niet al de waardes
-        Destiny_DAL.Character karakter = new Destiny_DAL.Character();*/
+
+
+        
+        Destiny_DAL.Character karakter  = new Destiny_DAL.Character();
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            /*
+
             cmbGezicht.ItemsSource = OphalenOpties("gezicht");
             cmbHaar.ItemsSource = OphalenOpties("haar");
             cmbGender.ItemsSource = OphalenOpties("gender");
             cmbMarking.ItemsSource = OphalenOpties("tattoo");
-
+            cmbRas.ItemsSource = OphalenRassen();
             cmbKlasse.ItemsSource = DatabaseOperations.OphalenCharacterKlasseVoorAanmaken();
             ///een subklasse kan niet gekozen worden zolang de klasse niet is gekozen
             cmbSubklasse.IsEnabled = false;
-            cmbRas.ItemsSource = OphalenRas();
-            
+
+
             ///knop aanmaken instellen bij het laden van het scherm
             btnAanmaken.IsEnabled = false;
 
-            */
-
-            
-           
         }
 
-        /*
-        public List<string>OphalenOpties(string beschrijving)
+        public List<string> OphalenOpties(string beschrijving)
         {
-            
-           List<CharacterCustomization> opties = DatabaseOperations.OphalenCharacterOptiesVoorAanmaken();
+
+
             List<string> lijst = new List<string>();
-              
-            foreach (CharacterCustomization item in opties)
+
+
+
+            if (beschrijving == "haar")
             {
-
-                if (beschrijving == "haar")
-                {
-                        lijst.Add(item.HeadOption); 
-                }
-
-                if (beschrijving == "tattoo")
-                {
-                    if (!lijst.Contains(item.Marking))
-                    {
-                        lijst.Add(item.Marking);
-                    }
-                    
-                }
-                if (beschrijving == "gezicht")
-                {
-                    if (!lijst.Contains(item.Face))
-                    {
-                        lijst.Add(item.Face);
-                    }
-                }
-                if (beschrijving == "gender")
-                {
-                    if (!lijst.Contains(item.Gender))
-                    {
-                        lijst.Add(item.Gender);
-                            ///in de database stond niets voor vrouw en nee wij zijn niet seksistisch///
-                            ///het was ook engels in de database "male"
-                        lijst.Add("female");
-                    }
-                }
-                
-                
+                lijst.Add("krullen");
+                lijst.Add("lang");
+                lijst.Add("kort");
             }
-            return lijst;
-           
-    }*/
 
-        ///methode die nagaat welke CharacterKlasseId moet worden gezocht in de tabel subklasse aangezien subklasse verschilt per klasse//
-        /*  private int OphalenSubklasses()
-          {
-
-              int id = 0;
-              if (cmbKlasse.SelectedItem is CharacterKlasse klasse)
-              {
-                  switch (klasse.Naam)
-                  {
-                      case "Titan":
-                           id = klasse.id;
-                          break;
-
-                      case "Hunter":
-                          id = klasse.id;
-                          break;
-
-                      case "Warlock":
-                          id = klasse.id;
-                          break;
-                  }
-              }
-              return id;
-    }*/
-
-        /*
-        private List <string> OphalenRas()
-        {
-
-            List<CharacterCustomization> rassen = DatabaseOperations.OphalenCharacterOptiesVoorAanmaken();
-            List<string> returnLijst = new List<string>();
-
-            foreach  (var item in rassen)
+            if (beschrijving == "tattoo")
             {
-                List<Ras> list = new List<Ras>();
+                lijst.Add("streep");
+                lijst.Add("geen marking");
+                lijst.Add("gezicht tattoo");
+            }
+            if (beschrijving == "gezicht")
+            {
+                lijst.Add("jong");
+                lijst.Add("oud");
+                lijst.Add("krijger");
+            }
+            if (beschrijving == "gender")
+            {
 
-                if (item.Ras.Naam == "Awoken" || item.Ras.Naam == "Human" || item.Ras.Naam == "Exo")
+                lijst.Add("Man");
+                lijst.Add("Vrouw");
+
+            }
+         
+               
+            return lijst;
+
+        }
+        private List<Ras>OphalenRassen()
+        {
+            List<Ras> rassen = DatabaseOperations.OphalenRasVoorAanmaken();
+            List<Ras> returnLijst = new List<Ras>();
+            foreach (Ras item in rassen)
+            {
+                if (item.Naam == "Awoken" || item.Naam == "Human" || item.Naam == "Exo")
                 {
-                    returnLijst.Add(item.Ras.Naam);
+                    returnLijst.Add(item);
                 }
             }
             return returnLijst;
-        }*/
+        }
 
 
-
-
-        private void btnAanmaken_Click(object sender, RoutedEventArgs e)
+        ///methode die nagaat welke CharacterKlasseId moet worden gezocht in de tabel subklasse aangezien subklasse verschilt per klasse//
+        private int OphalenSubklasses()
         {
-            /*
-           
-            if (customization != null)
+            int id = 0;
+            if (cmbKlasse.SelectedItem is CharacterKlasse klasse)
             {
-                karakter.CharacterCustomizationId = customization.id;
+                switch (klasse.Naam)
+                {
+                    case "Titan":
+                        id = klasse.id;
+                        break;
+
+                    case "Hunter":
+                        id = klasse.id;
+                        break;
+
+                    case "Warlock":
+                        id = klasse.id;
+                        break;
+                }
             }
-            
-   */
+            return id;
         }
 
-      
-        private void cmbRas_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-           
-        }
 
-        private void cmbSubklasse_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
 
-        }
 
-        private void cmbKlasse_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            /*
-            cmbSubklasse.IsEnabled = true;
-            cmbSubklasse.ItemsSource = DatabaseOperations.OphalenCharacterSubklasseVoorAanmaken(OphalenSubklasses());*/
-        }
+        
 
-        private void cmbGezicht_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void cmbGender_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void cmbHaar_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-        /*
+       
         private string ValideerComboboxen()
         {
-            
+           
             string melding = "";
-            
+
             if (cmbGender.SelectedIndex == -1)
             {
                 melding += "Selecteer een gender!" + Environment.NewLine;
@@ -227,76 +167,73 @@ namespace Project_Destiny_WPF.UserControls
             {
                 melding += "Selecteer een subklasse!" + Environment.NewLine;
             }
-            
-            
+
+
 
             return melding;
 
+        }
 
 
-    }*/
-
-        private void ComboboxItemsInstellenVoorBtnOpslagen()
+       
+        private void ComboboxItemsInstellenVoorBtnTonen()
         {
-            /*
+
             if (cmbGender.SelectedItem is string gender)
             {
-                customization.Gender = gender;
+                karakter.Gender = gender;
             }
             if (cmbGezicht.SelectedItem is string gezicht)
             {
-                customization.Face = gezicht;
+                karakter.Face = gezicht;
             }
             if (cmbHaar.SelectedItem is string haar)
             {
-                customization.HeadOption = haar;
+                karakter.HeadOption = haar;
             }
             if (cmbRas.SelectedItem is Ras ras)
             {
-                MessageBox.Show(ras.Naam + ras.id);
-                customization.RasId = ras.id;
-                MessageBox.Show("dit is de ras id");
+                karakter.RasId = ras.id;
+                karakter.Ras = ras;
             }
 
             if (cmbMarking.SelectedItem is string marking)
             {
-                customization.Marking = marking;
+                karakter.Marking = marking;
             }
-         */
+
         }
 
-        private void btnOpslagen_Click(object sender, RoutedEventArgs e)
+        private void cmbKlasse_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            /*
-            ComboboxItemsInstellenVoorBtnOpslagen();
 
-            string foutmelding = ValideerComboboxen();
+                cmbSubklasse.IsEnabled = true;
+                cmbSubklasse.ItemsSource = DatabaseOperations.OphalenCharacterSubklasseVoorAanmaken(OphalenSubklasses());
+            
+        }
 
-            if (string.IsNullOrWhiteSpace(foutmelding))
-            {
-                customization = new Destiny_DAL.CharacterCustomization();
-               
-                int ok = DatabaseOperations.InstellingenVanAanmakenOpslaanInDatabase(customization);
-                if (ok > 0)
-                {
-                    btnAanmaken.IsEnabled = true;
-                    MessageBox.Show("Instellingen zijn succesvol opgeslagen!");
-                    lstOverzicht.Items.Add("Haar: " + customization.HeadOption);
-                    lstOverzicht.Items.Add("Tattoo:" + customization.Marking);
-                    lstOverzicht.Items.Add("Gender: " + customization.Gender);
-                    lstOverzicht.Items.Add("Gezicht" + customization.Face);
-                    lstOverzicht.Items.Add("Ras" + customization.Ras);
-                }
-                else
-                {
-                    MessageBox.Show("fout bij het opslagen van de instellingen");
-                }
+        
 
-            }
-            else
-            {
-                MessageBox.Show(foutmelding);
-            }*/
+        private void btnToon_Click(object sender, RoutedEventArgs e)
+        {
+           
+            ComboboxItemsInstellenVoorBtnTonen();
+            List<string> attributen = new List<string>();
+              
+                attributen.Add("haar: " + karakter.HeadOption);
+                attributen.Add("ras: " + karakter.Ras.Naam);
+                attributen.Add("gezicht: " + karakter.Face);
+                attributen.Add("gender: " + karakter.Gender);
+                attributen.Add("marking: " + karakter.Marking);
+                lstToon.ItemsSource = attributen;
+                lstToon.Items.Refresh();
+                btnAanmaken.IsEnabled = true;
+            
+        }
+
+        private void btnAanmaken_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
-}
+    }
