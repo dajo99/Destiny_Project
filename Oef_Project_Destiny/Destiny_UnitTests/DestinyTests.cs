b65dc8edd0a3542cb7eff98583e8982fd85881f6
Gilles98 <guigilles@gmail.com>
2020-05-20 14:08:40 +0200

    first unit tests committed

--- Oef_Project_Destiny/Destiny_UnitTests/DestinyTests.cs
+++ Oef_Project_Destiny/Destiny_UnitTests/DestinyTests.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Destiny_Models;
 namespace Destiny_UnitTests
 {
     [TestClass]
     public class DestinyTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValideerAccountGegevens_WhenCalled_ReturnsEmptyString()
         {
+            ///arrange
+            string foutmelding = "";
+
+            ///act
+
+            foutmelding = Valideer.ValideerAccountGegevens("blabla", "wawawa", "gillesekegui@hotmail.com", "blabla");
+
+            ///assert
+            Assert.AreEqual("", foutmelding);
+        }
+
+
+        [TestMethod]
+        public void ValideerAccountGegevens_WhenCalled_ReturnsNotEmptyStringOnGebruikersNaam()
+        {
+            ///arrange
+            string foutmelding = "";
+
+            ///act
+
+            foutmelding = Valideer.ValideerAccountGegevens("blabla", "", "gillesekegui@hotmail.com", "blabla");
+
+            ///assert
+            Assert.IsTrue(foutmelding != "");
         }
     }
 }
